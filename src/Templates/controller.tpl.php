c1dcfc9b992193f553ca90f67f9cf603fe7d3616
detolath <39243313+detolath@users.noreply.github.com>
2019-01-02 08:46:07 +0000

    Controller without grid data
    Update generator controller for crud without grid data

--- src/Templates/controller.tpl.php
+++ src/Templates/controller.tpl.php
@@ -1,38 +1,49 @@
 <?php
-
 namespace [[appns]]Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use [[appns]]Http\Requests;
 use [[appns]]Http\Controllers\Controller;
-
 use [[appns]][[model_uc]];
 
 use DB;
 
 class [[controller_name]]Controller extends Controller
 {
-    //
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
-
-    public function index(Request $request)
+   	/**
+   	 * Display a listing of the resource.
+   	 *
+   	 * @return Response
+   	 */
+	public function index()
 	{
-	    return view('[[view_folder]].index', []);
+		$index = [[model_uc]]::all();
+	    return view('[[view_folder]].index', compact('index'));
 	}
 
-	public function create(Request $request)
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
 	{
-	    return view('[[view_folder]].add', [
-	        []
-	    ]);
+	    return view('[[view_folder]].add');
 	}
 
-	public function edit(Request $request, $id)
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
 	{
 		$[[model_singular]] = [[model_uc]]::findOrFail($id);
 	    return view('[[view_folder]].add', [
@@ -40,7 +51,13 @@ class [[controller_name]]Controller extends Controller
 	    ]);
 	}
 
-	public function show(Request $request, $id)
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
 	{
 		$[[model_singular]] = [[model_uc]]::findOrFail($id);
 	    return view('[[view_folder]].show', [
@@ -48,90 +65,58 @@ class [[controller_name]]Controller extends Controller
 	    ]);
 	}
 
-	public function grid(Request $request)
-	{
-		$len = $_GET['length'];
-		$start = $_GET['start'];
-
-		$select = "SELECT *,1,2 ";
-		$presql = " FROM [[prefix]][[tablename]] a ";
-		if($_GET['search']['value']) {	
-			$presql .= " WHERE [[first_column_nonid]] LIKE '%".$_GET['search']['value']."%' ";
-		}
-		
-		$presql .= "  ";
-
-		$sql = $select.$presql." LIMIT ".$start.",".$len;
-
-
-		$qcount = DB::select("SELECT COUNT(a.id) c".$presql);
-		//print_r($qcount);
-		$count = $qcount[0]->c;
-
-		$results = DB::select($sql);
-		$ret = [];
-		foreach ($results as $row) {
-			$r = [];
-			foreach ($row as $value) {
-				$r[] = $value;
-			}
-			$ret[] = $r;
-		}
-
-		$ret['data'] = $ret;
-		$ret['recordsTotal'] = $count;
-		$ret['iTotalDisplayRecords'] = $count;
-
-		$ret['recordsFiltered'] = count($ret);
-		$ret['draw'] = $_GET['draw'];
-
-		echo json_encode($ret);
-
-	}
-
-
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
 	public function update(Request $request) {
-	    //
-	    /*$this->validate($request, [
-	        'name' => 'required|max:255',
-	    ]);*/
+	    $this->validate($request, [
+	    	[[foreach:columns]]
+	        	'[[i.name]]' => 'required',
+	        [[endforeach]]
+	    ]);
+
 		$[[model_singular]] = null;
-		if($request->id > 0) { $[[model_singular]] = [[model_uc]]::findOrFail($request->id); }
+		if($request->_model) { $[[model_singular]] = [[model_uc]]::findOrFail($request->_model); }
 		else { 
 			$[[model_singular]] = new [[model_uc]];
 		}
-	    
-
+	  
 	    [[foreach:columns]]
-		
 		[[if:i.name=='id']]
-	    $[[model_singular]]->[[i.name]] = $request->[[i.name]]?:0;
+	    //$[[model_singular]]->[[i.name]] = $request->[[i.name]]?:0;
 		[[endif]]
 		[[if:i.name!='id']]
 	    $[[model_singular]]->[[i.name]] = $request->[[i.name]];
 		[[endif]]
-
 	    [[endforeach]]
-	    //$[[model_singular]]->user_id = $request->user()->id;
 	    $[[model_singular]]->save();
-
-	    return redirect('/[[route_path]]');
-
+	    return redirect()->action('[[model_uc]]Controller@index')->with('success','[[model_uc]] has been added.');
 	}
 
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
 	public function store(Request $request)
 	{
 		return $this->update($request);
 	}
 
-	public function destroy(Request $request, $id) {
-		
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id) {
 		$[[model_singular]] = [[model_uc]]::findOrFail($id);
-
 		$[[model_singular]]->delete();
-		return "OK";
+		return redirect()->action('[[model_uc]]Controller@index')->with('success', '[[model_uc]] has been deleted.');
 	    
 	}
-
 	
 }
